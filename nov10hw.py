f232665c90fb48bf34fbd824a7ce4f92aedfae5b
Ally Deeter <allydeeter6639@gmail.com>
2020-11-10 17:30:32 -0800

    small #2

--- nov10hw.py
+++ nov10hw.py
@@ -1,2 +1,9 @@
-name = input("What is your name?")
-print("Hello, ", name, "!", sep="")
+name = input("WHAT IS YOUR NAME? ")
+nameCap =  name.upper()
+    
+print ("HELLO, ", nameCap, "!\nYOUR NAME HAS ", len(name), " LETTERS IN IT! AWESOME!", sep="") 
+
+
+
+
+
